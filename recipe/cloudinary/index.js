1d0b126f5fcf5ca52bf9dfea62b15079c2e7b0a0
Samra <118079600+SamraAzizi@users.noreply.github.com>
2024-10-29 23:25:11 +0500

    Update index.js

--- recipe/cloudinary/index.js
+++ recipe/cloudinary/index.js
@@ -5,6 +5,15 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
+ });
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: 'YelpCamp',
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
+});
 
 
 module.exports = {
