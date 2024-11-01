b011e6aa84d4f6d8d27f8276ca07d1159b4af7d4
Samra <118079600+SamraAzizi@users.noreply.github.com>
2024-11-01 23:12:28 +0500

    Update cities.js

--- recipe/seeds/cities.js
+++ recipe/seeds/cities.js
@@ -8,6 +8,15 @@ module.exports = [
         rank: "1",
         state: "New York",
     },
+     {
+        city: "Los Angeles",
+        growth_from_2000_to_2013: "4.8%",
+        latitude: 34.0522342,
+        longitude: -118.2436849,
+        population: "3884307",
+        rank: "2",
+        state: "California",
+    },
     {
         city: "Los Angeles",
         growth_from_2000_to_2013: "4.8%",
